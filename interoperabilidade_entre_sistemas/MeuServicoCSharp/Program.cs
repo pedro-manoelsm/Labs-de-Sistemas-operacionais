9cbf9b9c2dee3e2aafc200fedd356b9107ed203e
Pedro Manoel SIlva Marques <pedrooo.marques02@gmail.com>
2025-10-04 20:07:41 -0300

    api em c# finalizada

--- interoperabilidade_entre_sistemas/MeuServicoCSharp/Program.cs
+++ interoperabilidade_entre_sistemas/MeuServicoCSharp/Program.cs
@@ -1,41 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Endpoint simples em C#
+app.MapGet("/dados", () => new { mensagem = "Olá do .NET!", linguagem = "C#" });
+
+// C# consumindo o serviço do Python
+app.MapGet("/consumir-python", async () =>
 {
-    app.MapOpenApi();
-}
+    using var httpClient = new HttpClient();
+    var response = await httpClient.GetFromJsonAsync<dynamic>("http://localhost:5000/dados");
+    return new { resposta_do_python = response };
+});
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+app.Run("http://localhost:5001");
